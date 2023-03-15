d232ce29d69914132c39272f279ad00d4d5667d0
Osvaldo <operez@cyberideas.com.mx>
2023-03-15 13:15:40 -0600

    NEW reprote save form

--- src/app/pages/reporte-fidelizacion/dashboard-reporte-fidelizacion/dashboard-reporte-fidelizacion.component.ts
+++ src/app/pages/reporte-fidelizacion/dashboard-reporte-fidelizacion/dashboard-reporte-fidelizacion.component.ts
@@ -3,6 +3,7 @@ import { UntypedFormBuilder,UntypedFormGroup, Validators } from '@angular/forms'
 import * as moment from 'moment';
 moment.lang('es');
 import { CorsService } from '@services';
+import { Message, MessageService } from 'primeng/api';
 
 
 
@@ -17,16 +18,16 @@ export class DashboardReporteFidelizacionComponent implements OnInit {
   today = new Date();
   result:any=[];
 
-  constructor(private formBuilder: UntypedFormBuilder,private cors: CorsService) { 
+  constructor(private messageService: MessageService,private formBuilder: UntypedFormBuilder,private cors: CorsService) { 
     this.formReporte = this.formBuilder.group({
       reporte: [null, Validators.required],
-      fechaini: [null, Validators.required],
-      fechafin: [null, Validators.required],
+      fechaini: [null],
+      fechafin: [null],
     });
     this.cors
     .get('Reporte/vici')
     .then((response) => {
-      // console.log(response)
+      console.log(response)
       this.result = response;
     //   this.messageService.add({
     //     key: 'tst',
@@ -59,13 +60,45 @@ export class DashboardReporteFidelizacionComponent implements OnInit {
   }
 
   enviar(){
+    this.formReporte.markAllAsTouched();
     console.log(this.formReporte)
     // this.cors.getCommand(`http://192.168.48.225:21?command=REBOOT`)
     // this.cors.getCommand(`http://192.168.48.225:80?command=REBOOT`)
-    this.cors.getCommand(`http://192.168.48.225:9000?command=REBOOT`)
+    // this.cors.getCommand(`http://192.168.48.225:9000?command=REBOOT`)
     // this.cors.getCommand(`http://192.168.50.58:9000?command=REBOOT`)
     // this.cors.getCommand(`http://192.168.61.4:9000?command=REBOOT`)
     // this.cors.getCommand(`http://20.51.210.241:9000?command=REBOOT`)
+    let a={
+      "id":0,
+      "Cve_usuario": "",
+      "list_id": `${this.formReporte.value.reporte}`,
+      "archivo": "",
+      "procesando": "",
+      "fechaCaptura": null,
+      "fechaCompletado": null,
+      "status": "",
+      "ip": "" 
+    }
+    this.cors.post('Reporte/GuardarFormularioEjecucionReporte',a)
+    .then((response) => {
+      console.log(response)
+      this.messageService.add({
+        key: 'tst',
+        severity: 'success',
+        summary: 'Datos guardados',
+        detail: 'La solicitud de cancelacion fue guardada',
+      });
+    })
+    .catch((error) => {
+      console.log(error)
+      this.messageService.add({
+        key:'tst',
+        severity: 'error',
+        summary: 'No se logro guardar',
+        detail: 'Intenta Nuevamente!!!',
+      });
+    });
   }
+
 
 }
