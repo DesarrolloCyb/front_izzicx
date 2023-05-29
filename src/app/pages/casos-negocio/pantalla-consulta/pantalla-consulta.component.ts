c3f87c2f297479747784a2fdd879a4fa666786a9
Osvaldo <operez@cyberideas.com.mx>
2023-05-29 13:56:12 -0600

    NEW notdone module

--- src/app/pages/casos-negocio/pantalla-consulta/pantalla-consulta.component.ts
+++ src/app/pages/casos-negocio/pantalla-consulta/pantalla-consulta.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Message,MessageService } from 'primeng/api';
+import { CorsService } from '@services';
+import * as moment from 'moment';
+import * as XLSX from 'xlsx';
+import { Table } from 'primeng/table';
+
 
 @Component({
   selector: 'pantalla-consulta',
@@ -6,10 +12,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pantalla-consulta.component.scss']
 })
 export class PantallaConsultaComponent implements OnInit {
-
-  constructor() { }
+  notdone:any[]=[];
+  constructor(
+    private cors: CorsService,
+    private messageService: MessageService,
+  ) { }
 
   ngOnInit(): void {
+    this.getTablaNotDone();
   }
+
+  getTablaNotDone(){
+    this.cors.get('AjustesNotDone/getAllEjecucionNotDone').then((response) => {
+      console.log(response)
+      if(response[0] == 'SIN INFO'){
+        this.notdone=[];
+      }else{
+        this.notdone=response;
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+  dateFormat(value:any){
+    // console.log(value)
+    if(value != null){
+      return moment(value).format('DD/MM/yyyy HH:mm:ss')
+    }else{
+      return ""
+    }
+  }
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+
 
 }
