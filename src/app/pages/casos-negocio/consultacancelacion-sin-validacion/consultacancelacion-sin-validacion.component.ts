f2b3fa0b11205f4c8097179a6eb967cbb47c4e20
Osvaldo <operez@cyberideas.com.mx>
2023-06-28 10:43:48 -0600

    ADD consulta para casos negocio sinvalidcion y cancelacionsinvalidcion

--- src/app/pages/casos-negocio/consultacancelacion-sin-validacion/consultacancelacion-sin-validacion.component.ts
+++ src/app/pages/casos-negocio/consultacancelacion-sin-validacion/consultacancelacion-sin-validacion.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Message,MessageService } from 'primeng/api';
+import { CorsService } from '@services';
+import * as moment from 'moment';
+import * as XLSX from 'xlsx';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'consultacancelacion-sin-validacion',
@@ -6,10 +11,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consultacancelacion-sin-validacion.component.scss']
 })
 export class ConsultacancelacionSinValidacionComponent implements OnInit {
+  cancelacion:any[]=[];
+  stats:any[]=[];
 
-  constructor() { }
+  constructor(
+    private cors: CorsService,
+    private messageService: MessageService,
+  ) { }
 
   ngOnInit(): void {
+    this.getTablaCancelacion();
+    this.statsCancelacion();
   }
 
+
+  getTablaCancelacion(){
+    this.cors.get('AjustesNotDone/getCancelacionSinValidacion').then((response) => {
+      // console.log(response)
+      if(response[0] == 'SIN INFO'){
+        this.cancelacion=[];
+      }else{
+        this.cancelacion=response;
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+  dateFormat(value:any){
+    // console.log(value)
+    if(value != null){
+      return moment(value).format('DD/MM/yyyy HH:mm:ss')
+    }else{
+      return ""
+    }
+  }
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+  statsCancelacion(){
+    this.cors.get('AjustesNotDone/getStatsCancelacionSinValidacion').then((response) => {
+      for (let i = 0; i < response.length; i++) {
+        const jsonObject = response[i];
+        for (let key in jsonObject) {
+          if (jsonObject.hasOwnProperty(key) && typeof jsonObject[key] === "object" && !Array.isArray(jsonObject[key])) {
+            jsonObject[key] = 0;
+          }
+        }
+      }      
+      this.stats=response;
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+  
 }
--- src/app/pages/casos-negocio/consultacasos-negocio-sin-validacion/consultacasos-negocio-sin-validacion.component.ts
+++ src/app/pages/casos-negocio/consultacasos-negocio-sin-validacion/consultacasos-negocio-sin-validacion.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Message,MessageService } from 'primeng/api';
+import { CorsService } from '@services';
+import * as moment from 'moment';
+import * as XLSX from 'xlsx';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'consultacasos-negocio-sin-validacion',
@@ -6,10 +11,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consultacasos-negocio-sin-validacion.component.scss']
 })
 export class ConsultacasosNegocioSinValidacionComponent implements OnInit {
+  casosNegocio:any[]=[];
+  stats:any[]=[];
 
-  constructor() { }
+  constructor(
+    private cors: CorsService,
+    private messageService: MessageService
+  ) { }
 
   ngOnInit(): void {
+    this.getTablaCasosNegocio();
+    this.statsCasosNegocio();
   }
+
+
+
+  getTablaCasosNegocio(){
+    this.cors.get('AjustesNotDone/getCasosNegocioSinValidacion').then((response) => {
+      // console.log(response)
+      if(response[0] == 'SIN INFO'){
+        this.casosNegocio=[];
+      }else{
+        this.casosNegocio=response;
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+  dateFormat(value:any){
+    // console.log(value)
+    if(value != null){
+      return moment(value).format('DD/MM/yyyy HH:mm:ss')
+    }else{
+      return ""
+    }
+  }
+  onGlobalFilter(table: Table, event: Event) {
+    table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+  }
+  statsCasosNegocio(){
+    this.cors.get('AjustesNotDone/getStatsCasosNegocioSinValidacion').then((response) => {
+      for (let i = 0; i < response.length; i++) {
+        const jsonObject = response[i];
+        for (let key in jsonObject) {
+          if (jsonObject.hasOwnProperty(key) && typeof jsonObject[key] === "object" && !Array.isArray(jsonObject[key])) {
+            jsonObject[key] = 0;
+          }
+        }
+      }      
+      this.stats=response;
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
+
 
 }
