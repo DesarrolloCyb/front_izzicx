96848ae71aae00dfe7f9683dd9c3413ad065ab3b
Osvaldo <osvaldocyber457@gmail.com>
2023-12-18 17:30:00 -0600

    change bases depuracion cc y ext en registro pendiente

--- src/app/pages/depuracion/bases-canceladas/bases-canceladas.component.ts
+++ src/app/pages/depuracion/bases-canceladas/bases-canceladas.component.ts
@@ -104,7 +104,7 @@ export class BasesCanceladasComponent implements OnInit {
         }
         if(count == 10){
           Object.keys(this.ExcelData).forEach(key => {
-            this.ExcelData[key]["Status"]='Pendiente';
+            this.ExcelData[key]["Status"]='Registro pendiente';
             this.ExcelData[key]["Cve_usuario"]=this.usuario.email;
             this.ExcelData[key]["Procesando"]="0";
             this.ExcelData[key]["IP"]="";
